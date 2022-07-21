e960a0d6bedb74205fdd33f9990103a74f4556b9
Akilah Snowden <akilahdsnowden@gmail.com>
2022-07-21 17:18:41 -0400

    React setup

---
+++ client/src/App.js
@@ -0,0 +1,60 @@
+// import logo from './logo.svg';
+import './App.css';
+import { UseState, useEffect } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    fetch("/hello")
+    .then((r) => r.json())
+    .then ((data) => setCount(data.count));
+}, []);
+
+  return (
+    <div className="App">
+      <h1>Page Count: {count}</h1>
+    </div>
+  );
+}
+
+export default App;
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
